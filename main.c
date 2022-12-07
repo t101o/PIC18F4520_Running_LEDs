1e1bf9745920604073a8682bf896caeff372f72a
Teodors Drazlovskis <teodors.drazlovskis@testdevlab.com>
2022-12-07 23:55:40 +0200

    Button press to change direction.

--- main.c
+++ main.c
@@ -1,35 +1,70 @@
 #include "p18cxxx.h"
 
 void delay();
+void change_port(unsigned char *port);
+__interrupt() void button_press(void);
+
+volatile unsigned char direction = 0x01; // 0x01 Right, 0x00 Left
 
 void delay() {
     int counter = 0;
-    for (counter = 0; counter<10000; counter++) {
+    for (counter = 0; counter<25000; counter++) {
         ;
     }
 }
+
+__interrupt() void button_press(void) {
+    if (INT0IF == 1) {
+        direction ^= 0x01;
+        INT0IF = 0;
+    }
+}
+
+void change_port(unsigned char *port) {
+    if (*port & 0x01) {
+        PORTC = 0x00;
+    } else {
+        PORTD = 0x00;
+    }
+    *port ^= 0x01;
+}
 // ASSUMPTION: There are 16 LEDs connected to the uController
-// TODO: Setup interrupts by the button to change direction of the glow.
-// TODO: Do more than 8 diodes with other ports
-//         IDEA: When one port reaches 0x80 use the next port pins.
-//               State could be put in a flag.
 void main(void) {
-    TRISB = 0;
+    // Initialization
+    TRISB = 1; // Set for input
     PORTB = 0x00;
+    INTCONbits.GIE = 1; // Enabled global interrupts
+    INTCONbits.INT0IF = 0; // Cleared INT0 interrupt flag
+    INTCONbits.INT0IE = 1; // Enabled INT0 interrupts
+    TRISC = 0; // Set for output
+    TRISD = 0; // Set for output
+    PORTC = 0x00;
+    PORTD = 0x00;
+    
     unsigned char i = 0x01; // cycles through the pins
-    unsigned char direction = 0x01; // 0x01 Right, 0x00 Left
+    unsigned char port = 0x01; // 0x01 C ports, 0x00 D ports
+    
+    // Program loop
     while (1) {
-        PORTB = i;
+        if (port & 0x01) {
+            PORTC = i;
+        } else {
+            PORTD = i;
+        }
+        
         delay();
+        
         if (direction & 0x01) {
             if (i & 0x80) { // last diode case
-                i = 0x01; // TODO switch to next port
+                change_port(&port);
+                i = 0x01;
             } else {
                 i = i << 1;
             }
         } else {
             if (i & 0x01) { // last diode case
-                i = 0x80; // TODO switch to next port
+                change_port(&port);
+                i = 0x80;
             } else {
                 i = i >> 1;
             }
